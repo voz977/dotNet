ce2c9eeb1c176cc9470c0b2c1ee1d6d038fbb223
Jonah <voz977@my.utsa.edu>
2018-08-11 18:51:50 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    var names = new List<string> { "Jonah", "Ana", "Felipe" };
+	    foreach (var name in names)
+	    {
+            	Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
